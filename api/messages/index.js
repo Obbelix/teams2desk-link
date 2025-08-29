e0a7aefbe3bc6f1e97cf413c9f41c2eacda371e0
Obbelix <61627537+Obbelix@users.noreply.github.com>
2025-08-29 14:01:00 +0200

    Update index.js

--- api/messages/index.js
+++ api/messages/index.js
@@ -10,7 +10,7 @@ const {
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
   MicrosoftAppId: process.env.MicrosoftAppId,
   MicrosoftAppPassword: process.env.MicrosoftAppPassword,
-  MicrosoftAppType: process.env.MicrosoftAppType || "MultiTenant",
+  MicrosoftAppType: process.env.MicrosoftAppType,
   MicrosoftAppTenantId: process.env.MicrosoftAppTenantId
 });
 
